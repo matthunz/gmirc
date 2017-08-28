7bc0e921770d7cc88d75f15dfb31bcf4edd4cdf7
matthunz <matthunz2@gmail.com>
2017-08-27 22:12:32 -0400

    Spawn a thread per connection

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
 
     for stream in listener.incoming() {
-        handle_client(stream.unwrap());
+        thread::spawn(move || {
+            handle_client(stream.unwrap());
+        });
     }
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
+    loop {
+        let mut buffer = [0; 512];
+        stream.read(&mut buffer).unwrap();
 
-    stream.write("test\r\n".as_bytes()).unwrap();
-    stream.flush().unwrap();
+        if stream.write("test\r\n".as_bytes()).is_err() {
+            break;
+        }
 
-    println!("{}", String::from_utf8_lossy(&buffer[..]))
+        println!("{}", String::from_utf8_lossy(&buffer[..]))
+    }
 }
