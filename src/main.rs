8e5d0a96a95a9f8dca870e297b8c6ae6854a7434
matthunz <matthunz2@gmail.com>
2017-08-27 21:13:04 -0400

    Create simple tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
+
+    for stream in listener.incoming() {
+        handle_client(stream.unwrap());
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    stream.write("test\r\n".as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("{}", String::from_utf8_lossy(&buffer[..]))
 }
