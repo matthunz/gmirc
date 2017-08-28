c91eb84e46af2a9d16cdb185f02f60e8a1e38735
matthunz <matthunz2@gmail.com>
2017-08-28 13:19:49 -0400

    Parse USER command and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::BufReader;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
@@ -15,33 +16,57 @@ fn main() {
 
 struct Client {
     stream: TcpStream,
-    nick: String
+    nick: String,
+    user: String,
+    hostname: String
 }
 
 impl Client {
     fn new(stream: TcpStream) -> Client {
-        Client { stream: stream, nick: String::new() }
+        Client {
+            stream: stream,
+            nick: String::new(),
+            user: String::new(),
+            hostname: String::new()
+        }
     }
 
     fn run(&mut self) {
         loop {
-            let mut buffer = [0; 512];
-            self.stream.read(&mut buffer).unwrap();
+            let mut buffer = BufReader::new(self.stream.try_clone().unwrap());
+            let mut msg = String::new();
 
-            let line = String::from_utf8(buffer.to_vec()).unwrap();
-            self.parse_command(&line);
+            match buffer.read_line(&mut msg) {
+                Ok(bytes) => {
+                    if bytes == 0 { break; }
 
-            let msg = format!(":gmirc 001 {0} :Welcome!\r {0}", self.nick);
-            if self.stream.write(msg.as_bytes()).is_err() {
-                break;
+                    print!("Received data ({} bytes): {}", bytes, msg); 
+                    self.parse_command(&msg);
+                } 
+                Err(_) => println!("Error receiving data")
             }
+
         }
-        self.nick = String::from("test2")
     }
 
     fn parse_command(&mut self, line: &str) {
         if line.contains("NICK") {
-            self.nick = String::from(line.split_at(5).1)
+            self.nick = line.split_at(5).1.to_owned();
+
+            // remove \r\n
+            let new_len = self.nick.len() - 2;
+            self.nick.truncate(new_len);
+
+        } else if line.contains("USER") {
+            let tokens:  Vec<&str> = line.split(" ").collect();
+
+            self.user = tokens[1].to_owned();
+            self.hostname = tokens[2].to_owned();
+
+            // welcome the newly defined user
+            let msg = format!(":gmirc 001 {0} :Welcome! <{0}>[!<{1}>@<{2}>]\r\n",
+                              self.nick, self.user, self.hostname);
+            self.stream.write(msg.as_bytes()).unwrap();
         }
     }
 }
