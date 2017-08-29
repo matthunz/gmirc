0b4e70e630997dc843bca755cc23a57bdc4dbeb5
matthunz <matthunz2@gmail.com>
2017-08-28 23:36:09 -0400

    Rename client to connection

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::net::TcpListener;
 use std::thread;
+use connection::Connection;
 
-mod client;
+mod connection;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6667").unwrap();
 
     for stream in listener.incoming() {
         thread::spawn(move || {
-            let mut client = client::Client::new(stream.unwrap());
-            client.run();
+            let mut c = Connection::new(stream.unwrap());
+            c.run();
         });
     }
 }
